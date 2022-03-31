b7bc5784b6f420dbb7de1868b3c4e38b337760bd
spti <tobetodee@gmail.com>
2022-03-31 13:38:17 +0300

    describe the desired output in the demo functions

--- demo/to-tree.js
+++ demo/to-tree.js
@@ -14,6 +14,24 @@ function mergePathsOfNamedNodes() {
 
     console.log("mergePathsOfNamedNodes, tree", tree);
 
+    // tree should be
+    // {
+    //     node: {
+    //         a: {
+    //             errors: [],
+    //             node: {
+    //                 b: {
+    //                     erorrs: [],
+    //                     node: {
+    //                         c: {errors: [{instancePath: "/a/b/c"}], node: null},
+    //                         d: {errors: [{instancePath: "/a/b/d"}], node: null},
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
     return tree
 }
 
@@ -31,6 +49,21 @@ function mergePathsWithArrItem() {
 
     console.log("mergePathsOfNamedNodes, tree", tree);
 
+    // tree should be
+    // {
+    //     node: {
+    //         a: {
+    //             errors: [],
+    //             node: [
+    //                 {index: 0, errors: [], node: {
+    //                     c: {errors: [{instancePath: "/a/0/c"}], node: null},
+    //                     b: {errors: [{instancePath: "/a/0/d"}], node: null}
+    //                 }}
+    //             ]
+    //         }
+    //     }
+    // }
+
     return tree
 }
 
@@ -47,6 +80,24 @@ function mergePathsWithArrItems() {
     const tree = toTree(errors)
 
     console.log("mergePathsOfNamedNodes, tree", tree);
+
+    // tree should be
+    // {
+    //     node: {
+    //         a: {
+    //             errors: [],
+    //             node: [{
+    //                 index: 0, errors: [],
+    //                 node: [
+    //                     {index: 0, errors: [], node: {
+    //                         c: {errors: [{instancePath: "/a/0/0/c"}], node: null},
+    //                         d: {errors: [{instancePath: "/a/0/0/d"}], node: null}
+    //                     }}
+    //                 ]
+    //             }]
+    //         }
+    //     }
+    // }
 
     return tree
 }
@@ -66,9 +117,101 @@ function paramsToTree() {
 
     const tree = toTree(errors)
 
-    console.log("mergePathsOfNamedNodes, tree", tree);
+    console.log("paramsToTree, tree", tree);
+
+    // tree should be
+    // {
+    //     node: {
+    //         a: {
+    //             erorrs: [],
+    //             node: {
+    //                 b: {
+    //                     errors: [],
+    //                     node: {
+    //                         c: {errors: [{instancePath: "/a/b", params: {missingProperty: "c"}}], node: null},
+    //                         d: {errors: [{instancePath: "/a/b/d"}], node: null}
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
     return tree
+}
+
+function samePathErrors() {
+    const errors = [
+        {instancePath: '/a/b', v: 'b0'},
+        {instancePath: '/a/b', v: 'b1'}
+    ]
+
+    const tree  = toTree(errors)
+    console.log("samePathErrors, tree", tree);
+
+    // tree should be:
+    // {
+    //     node: {
+    //         a: {
+    //             errors: [],
+    //             node: {
+    //                 errors: [],
+    //                 node: {
+    //                     b: {
+    //                         errors: [
+    //                             {instancePath: '/a/b', v: 'b0'},
+    //                             {instancePath: '/a/b', v: 'b1'}
+    //                         ],
+    //                         node: null
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+function samePathErrorsWithArrItems() {
+    const errors = [
+        {instancePath: '/a/0', v: 'b0'},
+        {instancePath: '/a/0', v: 'b1'}
+    ]
+
+    const tree  = toTree(errors)
+    console.log("samePathErrorsWithArrItems, tree", tree);
+
+    // tree should be:
+    // {
+    //     node: {
+    //         a: {
+    //             errors: [],
+    //             node: {
+    //                 errors: [],
+    //                 node: [
+    //                     {
+    //                         index: 0,
+    //                         errors: [
+    //                             {instancePath: '/a/0', v: 'b0'},
+    //                             {instancePath: '/a/0', v: 'b1'}
+    //                         ]
+    //                     }
+    //                 ]
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+function multipleLevelsErrors() {
+    // /a/b, /a/b/c
+}
+
+function conflictingNodes() {
+    // /a/b/c, /a/0/d; /a/0/c, /a/b/d
+}
+
+function emptyNodeName() {
+    // ///, /a//c
 }
 
 module.exports = {
